d89fd354a0f4da385b4b5f05ded14890430febd5
LaraeLua <111380182+LaraeLua@users.noreply.github.com>
2022-12-05 10:08:40 -0300

    Add files via upload

---
+++ Conexao.java
@@ -0,0 +1,17 @@
+
+import java.sql.*;
+
+public class Conexao {
+
+	static String status ="...";
+	
+	public static Connection getConnection() {
+		return null;
+		
+try {
+	Class.forName("com.mysql.jdbc.Driver").newInstance();
+	
+	String url = "jdbc:mysql"
+}
+	}
+}
